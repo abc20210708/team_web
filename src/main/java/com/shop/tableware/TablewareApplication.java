9e12e0c3759271f8c4215097d9f27947f8853529
abc20210708 <gotqlc2670@naver.com>
2022-02-19 19:40:02 +0900

    build.gradle 설정

---
+++ src/main/java/com/shop/tableware/TablewareApplication.java
@@ -0,0 +1,13 @@
+package com.shop.tableware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablewareApplication.class, args);
+	}
+
+}
